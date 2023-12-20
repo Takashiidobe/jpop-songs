f41c55519f26b926e4c72628424ad73f3bede145
Takashi Idobe <mail@takashiidobe.com>
2023-12-20 12:09:36 -0500

    update and add prometheus metrics

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,33 @@
-use axum::{
-    routing::{get},
-    Json, Router,
-    http::StatusCode,
-    response,
-    response::{IntoResponse, Response},
-    body::{self, Full}
-};
 use askama::Template;
-use std::net::SocketAddr;
+use axum::{
+    http::StatusCode,
+    response::{Html as HtmlResponse, IntoResponse, Response},
+    routing::get,
+    Json, Router,
+};
+use axum_prometheus::PrometheusMetricLayer;
 use scraper::{Html, Selector};
 
-use serde::{Serialize};
+use serde::Serialize;
 
 const CHARTS_URL: &str = "https://www.billboard.com/charts/japan-hot-100";
 const PORT: u16 = 3001;
 
 #[tokio::main]
 async fn main() {
+    let (prometheus_layer, metric_handle) = PrometheusMetricLayer::pair();
     tracing_subscriber::fmt::init();
 
     let app = Router::new()
         .route("/", get(root))
-        .route("/api", get(api));
+        .route("/api", get(api))
+        .route("/metrics", get(|| async move { metric_handle.render() }))
+        .layer(prometheus_layer);
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], PORT));
-    tracing::debug!("listening on {}", addr);
-    axum::Server::bind(&addr)
-        .serve(app.into_make_service())
+    let listener = tokio::net::TcpListener::bind(&format!("0.0.0.0:{}", PORT))
         .await
         .unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
 
 async fn root() -> impl IntoResponse {
@@ -43,7 +42,7 @@ async fn api() -> impl IntoResponse {
 }
 
 async fn get_songs() -> Vec<Song> {
-        let html = reqwest::get(CHARTS_URL)
+    let html = reqwest::get(CHARTS_URL)
         .await
         .unwrap()
         .text()
@@ -53,35 +52,27 @@ async fn get_songs() -> Vec<Song> {
     let fragment = Html::parse_fragment(&html);
     let selector = Selector::parse(".c-title.a-no-trucate").unwrap();
 
-    let mut titles = vec![];
-
-    for element in fragment.select(&selector) {
-        titles.push(element.inner_html());
-    }
-
-    let titles: Vec<_> = titles.into_iter().map(|x| x.trim().to_string()).collect();
+    let titles: Vec<_> = fragment
+        .select(&selector)
+        .map(|x| x.inner_html().trim().to_string())
+        .collect();
 
     let fragment = Html::parse_fragment(&html);
     let selector = Selector::parse(".c-label.a-no-trucate").unwrap();
 
-    let mut artists = vec![];
-
-    for element in fragment.select(&selector) {
-        artists.push(element.inner_html());
-    }
-
-    let artists: Vec<String> = artists.into_iter().map(|x| x.trim().to_string()).collect();
+    let artists: Vec<_> = fragment
+        .select(&selector)
+        .map(|e| e.inner_html().trim().to_string())
+        .collect();
 
     let mut songs = vec![];
 
     for (index, (artist, title)) in artists.into_iter().zip(titles.into_iter()).enumerate() {
-        songs.push(
-            Song {
-                rank: (index + 1) as u8,
-                song: title,
-                artist,
-            }
-        )
+        songs.push(Song {
+            rank: (index + 1) as u8,
+            song: title,
+            artist,
+        })
     }
 
     songs
@@ -97,7 +88,7 @@ struct Song {
 #[derive(Template)]
 #[template(path = "songs.html")]
 struct Songs {
-    songs: Vec<Song>
+    songs: Vec<Song>,
 }
 
 struct HtmlTemplate<T>(T);
@@ -108,14 +99,12 @@ where
 {
     fn into_response(self) -> Response {
         match self.0.render() {
-            Ok(html) => response::Html(html).into_response(),
-            Err(err) => Response::builder()
-                .status(StatusCode::INTERNAL_SERVER_ERROR)
-                .body(body::boxed(Full::from(format!(
-                    "Failed to render template. Error: {}",
-                    err
-                ))))
-                .unwrap(),
+            Ok(html) => HtmlResponse(html).into_response(),
+            Err(err) => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("Failed to render template. Error: {err}"),
+            )
+                .into_response(),
         }
     }
 }
